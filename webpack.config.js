04e8c6ee5124ed7cabc95f786c83217c59c51bf0
rashmi <rashmi.mallappa14@gmail.com>
2020-12-16 19:44:13 -0700

    setup webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,49 @@
 //entry-->output
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-    entry:'./src/app.js',
-    output:{                                          //configuring webpack
-        path:path.join(__dirname,'public'),
-        filename:'bundle.js'
-    },
-    module:{                                          //setting up babel with webpack
-        rules:[{
-            loader:'babel-loader',
-            test:/\.js$/,
-            exclude:/node_modules/
-        },{
-            test:/\.s?css$/,                          //setting up scss with webpack
-            use :[                                    //use - array of loaders
-                'style-loader',                       // takes css in JS representation and add into DOM
-                'css-loader',                         //allow webpack to load css into JS representation
-                'sass-loader'                         //behind the scene sass-loader uses node-sass to conver the file scss into css
-            ]
-        }]
-    },
-    devtool:'cheap-module-eval-source-map',           //replacing live-server with webpack-dev-server - this stores bundle.js in memory and doesn't create physical copy in the folder.
-    devServer:{
-        contentBase:path.join(__dirname,'public'),
-        historyApiFallback:true                      //tells dev server that routing is handled by client code
+module.exports = (env)=>{
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css')
+
+    return {
+        entry:'./src/app.js',
+        output:{                                          //configuring webpack
+            path:path.join(__dirname,'public'),
+            filename:'bundle.js'
+        },
+        module:{                                          //setting up babel with webpack
+            rules:[{
+                loader:'babel-loader',
+                test:/\.js$/,
+                exclude:/node_modules/
+            },{
+                test:/\.s?css$/,                          //setting up scss with webpack
+                use : CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options:{
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',           //replacing live-server with webpack-dev-server - this stores bundle.js in memory and doesn't create physical copy in the folder.
+        devServer:{
+            contentBase:path.join(__dirname,'public'),
+            historyApiFallback:true                      //tells dev server that routing is handled by client code
+        }
     }
 }
